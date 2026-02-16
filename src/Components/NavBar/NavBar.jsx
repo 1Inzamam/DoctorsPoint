4102cc1853b43011b50484e7ff651e245b6a3e0f
1Inzamam <inzamamkhan618@gmail.com>
2026-02-16 15:35:14 +0600

    Added navigation bar from daisy ui and customised dynamic buttons

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
 const NavBar = () => {
+  const links = (
+    <>
+      <Link>
+        <li>
+          <a>Home</a>
+        </li>
+      </Link>
+      <Link>
+        <li>
+          <a>My-Bookings</a>
+        </li>
+      </Link>
+      <Link>
+        <li>
+          <a>Blogs</a>
+        </li>
+      </Link>
+      <Link>
+        <li>
+          <a>Contact Us</a>
+        </li>
+      </Link>
+    </>
+  );
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
