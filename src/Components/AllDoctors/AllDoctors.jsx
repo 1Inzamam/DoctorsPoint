1720d9b716085ceac7073b25f3e6d974c51a4b9a
1Inzamam <inzamamkhan618@gmail.com>
2026-02-16 21:40:48 +0600

    rendering only 4 doctors cards untill the view all doctors button clicked

--- src/Components/AllDoctors/AllDoctors.jsx
+++ src/Components/AllDoctors/AllDoctors.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import SingleDoctor from "../SingleDoctor/SingleDoctor";
 
 const AllDoctors = ({ allDoctorsData }) => {
   const doctors = allDoctorsData.doctors;
+
+  const [showAll, setShowAll] = useState(false);
+
+
+  const doctorsToShow = showAll ? doctors : doctors.slice(0, 4);
+
   return (
     <div className="my-12">
       <div className="text-center mb-12 space-y-4">
@@ -10,9 +16,18 @@ const AllDoctors = ({ allDoctorsData }) => {
         <p className="font-normal text-basez">Find the best doctors in our clinic</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
-        {doctors.map((doctor) => (
+        {doctorsToShow.map((doctor) => (
           <SingleDoctor key={doctor.id} doctor={doctor}></SingleDoctor>
         ))}
+      </div>
+      <div className="text-center mt-8">
+        {
+          doctors.length>4&& !showAll && (
+            <div>
+              <button onClick={()=>setShowAll(true)} className="px-6 py-3 bg-green-600 text-white rounded-4xl hover:bg-green-700 transition">View All Doctors</button>
+            </div>
+          )
+        }
       </div>
     </div>
   );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
   return (
     <div className='max-w-7xl mx-auto'>
         <AllDoctors allDoctorsData={allDoctorsData}></AllDoctors>
+        
     </div>
   )
 }
